056ef4a5aadb8d37d1543e26a0b51183f981d991
Luis Fernando Campos <systems4@logiztikalliance.com>
2024-02-16 13:45:55 -0500

    Refactoring Consumer

---
+++ Queues/MicroQueue.Consumer.Api/Controllers/ConsumerController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiConsumerQueue.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumerController : ControllerBase
+    {
+    }
+}
